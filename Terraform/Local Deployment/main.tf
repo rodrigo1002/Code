922b430419667909e1162edd23e7293623953564
Rodrigo <rodrigo1002@hotmail.com>
2024-03-07 20:32:48 -0300

    New EC2 File

--- Terraform/Local Deployment/main.tf
+++ Terraform/Local Deployment/main.tf
@@ -5,28 +5,28 @@ provider "hyperv" {
 
 # Define a virtual machine resource
 resource "hyperv_virtual_machine" "example_vm" {
-  name            = var.vm_name
+  name           = var.vm_name
   vm_switch_name = var.switch_name
 
   dynamic_memory {
-    enabled             = true
+    enabled                  = true
     maximum_bytes_per_second = 10000000
     minimum_bytes_per_second = 10000000
-    buffer_percentage   = 20
-    weight              = 75
+    buffer_percentage        = 20
+    weight                   = 75
   }
 
   processor_count = 2
   memory          = 2048
 
   dynamic "disk" {
-  for_each = [1]
-  content {
-    path   = "C:\\Code\\GitHub\\Terraform\\Local Deployment\\VM\\${var.vm_name}.vhdx"
-    size   = 20 # Size in GB
-    format = "vhdx"
+    for_each = [1]
+    content {
+      path   = "C:\\Code\\GitHub\\Terraform\\Local Deployment\\VM\\${var.vm_name}.vhdx"
+      size   = 20 # Size in GB
+      format = "vhdx"
+    }
   }
-}
 
   dynamic "network_interface" {
     for_each = [1]
